6c5f79e4c9440f0795d70a45c180740a1634146c
Adeshina Smart <adeshinasmart@gmail.com>
2025-01-15 16:34:57 +0100

    added allowed hosts for railway

--- mylife/settings.py
+++ mylife/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = 'django-insecure-keys2nj$@!6_o#x8s&2_0^9o(t5=_&&an&x)h)*f+qr56_g6j5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS =[]
+ALLOWED_HOSTS = ['mylife-production.up.railway.app','https://mylife-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS =['mylife-production.up.railway.app','https://mylife-production.up.railway.app']
 
 
 # Application definition
